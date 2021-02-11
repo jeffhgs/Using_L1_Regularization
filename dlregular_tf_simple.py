e0b2c6570da74f29856bdf7e78e9cc97470d2e7e
Jeff Henrikson <jeff@groovescale.com>
2021-02-11 11:37:41 -0800

    refactor: move model setup into run_train

--- dlregular_tf_simple.py
+++ dlregular_tf_simple.py
@@ -3,39 +3,39 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
 
-inputs = keras.Input(shape=(784,), name="digits")
-x1 = layers.Dense(64, activation="relu")(inputs)
-x2 = layers.Dense(64, activation="relu")(x1)
-outputs = layers.Dense(10, name="predictions")(x2)
-model = keras.Model(inputs=inputs, outputs=outputs)
-
-# Instantiate an optimizer.
-optimizer = keras.optimizers.SGD(learning_rate=1e-3)
-# Instantiate a loss function.
-loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-
-# Prepare the training dataset.
-batch_size = 64
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-x_train = np.reshape(x_train, (-1, 784))
-x_test = np.reshape(x_test, (-1, 784))
-
-# Reserve 10,000 samples for validation.
-x_val = x_train[-10000:]
-y_val = y_train[-10000:]
-x_train = x_train[:-10000]
-y_train = y_train[:-10000]
-
-# Prepare the training dataset.
-train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
-
-# Prepare the validation dataset.
-val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
-val_dataset = val_dataset.batch(batch_size)
-
 
 def run_train():
+    inputs = keras.Input(shape=(784,), name="digits")
+    x1 = layers.Dense(64, activation="relu")(inputs)
+    x2 = layers.Dense(64, activation="relu")(x1)
+    outputs = layers.Dense(10, name="predictions")(x2)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+
+    # Instantiate an optimizer.
+    optimizer = keras.optimizers.SGD(learning_rate=1e-3)
+    # Instantiate a loss function.
+    loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    # Prepare the training dataset.
+    batch_size = 64
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    x_train = np.reshape(x_train, (-1, 784))
+    x_test = np.reshape(x_test, (-1, 784))
+
+    # Reserve 10,000 samples for validation.
+    x_val = x_train[-10000:]
+    y_val = y_train[-10000:]
+    x_train = x_train[:-10000]
+    y_train = y_train[:-10000]
+
+    # Prepare the training dataset.
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
+
+    # Prepare the validation dataset.
+    val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+    val_dataset = val_dataset.batch(batch_size)
+
     epochs = 2
     for epoch in range(epochs):
         print("\nStart of epoch %d" % (epoch,))
