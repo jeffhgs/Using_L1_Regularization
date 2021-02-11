9a2d3c06939859f05e6b76ff8e58b08dbaf4048e
Jeff Henrikson <jeff@groovescale.com>
2021-02-11 11:39:01 -0800

    rename: _val -> _test

--- dlregular_tf_simple.py
+++ dlregular_tf_simple.py
@@ -14,14 +14,14 @@ def run_train(options):
 
     # Prepare the training dataset.
     batch_size = 64
-    x_train, x_val, y_train, y_val = setup_mnist_data()
+    x_train, x_test, y_train, y_test = setup_mnist_data()
 
     # Prepare the training dataset.
     train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
     train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
 
     # Prepare the validation dataset.
-    val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+    val_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
     val_dataset = val_dataset.batch(batch_size)
 
     epochs = 2
@@ -66,11 +66,11 @@ def setup_mnist_data():
     x_train = np.reshape(x_train, (-1, 784))
     x_test = np.reshape(x_test, (-1, 784))
     # Reserve 10,000 samples for validation.
-    x_val = x_train[-10000:]
-    y_val = y_train[-10000:]
+    x_test = x_train[-10000:]
+    y_test = y_train[-10000:]
     x_train = x_train[:-10000]
     y_train = y_train[:-10000]
-    return x_train, x_val, y_train, y_val
+    return x_train, x_test, y_train, y_test
 
 
 def build_model(options):
